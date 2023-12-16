888abef8cb5476227d33dc7b2ca085d5d518c779
wxsabi <35592749+wxsabi@users.noreply.github.com>
2023-12-16 15:56:15 -0800

    Missing "AS" & ORDER BY --> Character_items

--- week7/answers.sql
+++ week7/answers.sql
@@ -86,11 +86,11 @@ SELECT
     i.armor,
     i.damage
 FROM 
-    characters c
+    characters AS c
 INNER JOIN 
-    inventory inv ON c.character_id = inv.character_id -- not sure if this is correct
-INNER JOIN 
-    items i ON inv.item_id = i.item_id
+    inventory AS inv ON c.character_id = inv.character_id -- not sure if this is correct
+INNER JOIN                                                -- UPDATE: It wasn't XD
+    items AS i ON inv.item_id = i.item_id
 UNION
 SELECT 
     c.character_id,
@@ -99,11 +99,12 @@ SELECT
     i.armor,
     i.damage
 FROM 
-    characters c
+    characters AS c
 INNER JOIN 
-    equipped e ON c.character_id = e.character_id
+    equipped AS e ON c.character_id = e.character_id
 INNER JOIN 
-    items i ON e.item_id = i.item_id; -- this is where it should allow the view
+    items AS i ON e.item_id = i.item_id; -- this is where it should allow the view
+ORDER BY item_name;
 
 
 -- basically rinse repeat but for all of them in a team
