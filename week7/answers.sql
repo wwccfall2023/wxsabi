4a7e5e7fb2a3912a150b1def65bd42062c01ad80
wxsabi <35592749+wxsabi@users.noreply.github.com>
2023-12-16 13:26:35 -0800

    create first table "players"
    Only testing how this works

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,11 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players
+(
+  player_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  email VARCHAR(30)
+);
